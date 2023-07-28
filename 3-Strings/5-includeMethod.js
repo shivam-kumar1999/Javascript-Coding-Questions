cfc72e342b23b2c97eb30a13b70b0436e9e97a70
Shivam kumar <shivamkumarhost@gmail.com>
2023-07-29 00:03:49 +0530

    string

---
+++ 3-Strings/5-includeMethod.js
@@ -0,0 +1,6 @@
+//Include method will always return a boolean value..
+
+const message= "i like to code in light mode";
+console.log(message.includes("dark"));
+
+console.log(message.includes("light"));
---
+++ 3-Strings/6-lowerUppercase.js
@@ -0,0 +1,8 @@
+//toupperCase ------> to convert in uppercase.
+//toLowerCase ------> to convert in lowercase.
+
+
+let bio = "I am shivam kumar and i am 250 years old";
+
+console.log(bio.toUpperCase());
+console.log(bio.toLowerCase());
