ddf38e17cc6fbc05a716261b123344d1f1ac3457
Shivam kumar <99277501+shivam-kumar1999@users.noreply.github.com>
2023-11-25 11:41:59 +0530

    Update 16th.js

--- Js Practice Question/16th.js
+++ Js Practice Question/16th.js
@@ -1,2 +1,23 @@
 //write a program to print the fibonacci series up to a given number..
 
+
+let a=0;
+let b=1;
+let c;
+
+function fibo(num){
+    
+    for(let i=0; i<num; i++){
+        
+        console.log(a);
+        c=a+b
+        a=b;
+        b=c
+        
+    
+    }
+    
+}
+
+console.log(fibo(6))
+
