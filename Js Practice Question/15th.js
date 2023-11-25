2acf186d8f23d91b9676ea12fcfa4a32a454fffa
Shivam kumar <99277501+shivam-kumar1999@users.noreply.github.com>
2023-11-25 11:28:47 +0530

    Update 15th.js

--- Js Practice Question/15th.js
+++ Js Practice Question/15th.js
@@ -1,13 +1,16 @@
 //write a program to give factorial of a given number ;
 
 
-let first=0;
-let second=1;
-
-let fact=0;
-
-function factorial( num){
 
 
+function factorial(num){
     
+    let result=1;
+    
+    for(let i=1; i<=num; i++){
+        result *=i
+    }
+    return result
 }
+
+console.log(factorial(5))
